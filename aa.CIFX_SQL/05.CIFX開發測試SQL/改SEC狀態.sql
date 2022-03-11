a6436ef1125624dab610f853dd38117c6f026610
rjchenl <kf135117@gmail.com>
2022-03-11 13:05:30 +0800

    update 0311 新增本機jdk oms test 憑證

--- aa.CIFX_SQL/05.CIFX開發測試SQL/改SEC狀態.sql
+++ aa.CIFX_SQL/05.CIFX開發測試SQL/改SEC狀態.sql
@@ -6,16 +6,41 @@ SET EXECUTION_STATE     = 'TO_EXECUTE',
     ap_server_EXECUTING = NULL
 WHERE SERVICE_EXECUTION_CONTROL_ID IN ('1636448165161664-4283243-08-01')
   AND EXECUTION_TYPE = 'STAGE1_EXECUTION';
-
+/
 
 UPDATE CIFX.TB_SERVICE_EXECUTION_CONTROL
 SET EXECUTION_STATE     = 'EXECUTED',
     ap_server_EXECUTING = NULL
 WHERE SERVICE_EXECUTION_CONTROL_ID IN ('1636551271613337-4510181-44-01')
   and execution_type = 'STAGE1_EXECUTION';
-
+/
 
 SELECT EXECUTION_TYPE, SERVICE_INTERCHANGE_ID, CIRCI_KEY, EXECUTION_STATE
 FROM CIFX.TB_SERVICE_EXECUTION_CONTROL
 WHERE SERVICE_EXECUTION_CONTROL_ID IN ('1636448165161664-4283243-08-01')
   AND EXECUTION_TYPE = 'STAGE1_EXECUTION';
+/
+
+-- ======================== oms ============================
+
+
+select * from cifx.tb_oms_execution_control
+order by to_exec_timestamp desc
+;
+/
+
+update cifx.tb_oms_execution_control
+set ap_server_to_exec = null,
+ap_server_executing = null,
+ap_server_executing_thread = null,
+execution_state  ='TO_EXECUTE',
+to_exec_timestamp = NULL,
+executing_timestamp = null,
+executed_timestamp = null,
+broken_timestamp = null,
+retry_count = null,
+result_code = null
+where uuid = '1646844061267203-9665563-88-01';
+/
+commit;
+/
